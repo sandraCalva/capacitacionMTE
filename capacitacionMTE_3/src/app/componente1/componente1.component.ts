0c5f3f9cbf9611ba1983a84f4f5e86051e343f20
sandraCalva <sandra.calva.msc@gmail.com>
2022-09-01 22:30:36 +0200

    commit de console impresion

--- capacitacionMTE_3/src/app/componente1/componente1.component.ts
+++ capacitacionMTE_3/src/app/componente1/componente1.component.ts
@@ -10,6 +10,16 @@ export class Componente1Component implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.primeraFuncionSuma();
+  }
+
+  primeraFuncionSuma() {
+    console.log("estoy sumando" + "1+2");
+    console.info("soy un info");
+    console.log("soy un log");
+    console.warn("soy un warn");
+    console.error("soy un error");
+
   }
 
 }
