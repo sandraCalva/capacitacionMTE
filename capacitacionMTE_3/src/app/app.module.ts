2813d22970be2e9d5e961ceb980edae049067dbd
sandraCalva <sandra.calva.msc@gmail.com>
2022-09-01 23:04:30 +0200

    instalamos primeNG

--- capacitacionMTE_3/src/app/app.module.ts
+++ capacitacionMTE_3/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Componente1Component } from './componente1/componente1.component';
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Componente1Component
+    Componente1Component,
+    
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
